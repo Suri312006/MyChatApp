ccff32e4b919b1e29bd73700a43333f7c165f9b0
Surendra Jammishetti <suri312006@gmail.com>
2023-12-14 22:47:30 -0800

    bestest logic for conversation handling!!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,50 +18,60 @@
 	const select_user_to_message = async (id: string) => {
 		//create conversation if it doesnt exist already
 
-
 		const myID = data.session?.user.id;
 		const otherID = id;
 
-		let arr1 = await data.supabase.from('conversations').select().eq('user1', myID!);
-		let arr2 = await data.supabase.from('conversations').select().eq('user2', myID!);
-
-		// conversations that only currently logged in user is participating in
-		let conversations = arr1.data?.concat(arr2.data!)
-		
-		
-		//* okay wow that is dumb behavior, do not use forEach loops on async objects lmfao
-		for ( const conversation of conversations!) {
-			if (
-				(conversation.user1 == myID || conversation.user2 == myID) &&
-				(conversation.user2 == myID || conversation.user1 == otherID)
-			) {
-				// conversation exists, should return messages related to said conversation
-				console.log('conversation exists, fetching messages')
-				messages = await data.supabase.from('messages').select().eq('conversation_id', conversation.id);
-				return null
-			}
-		}
-		
-
-		console.log("creating new conversation")
-		//conversation does not exist atp
-		const { error } = await data.supabase
+		let arr1 = await data.supabase
 			.from('conversations')
-			.insert({ user1: myID, user2: otherID });
+			.select()
+			.eq('user1', myID!)
+			.eq('user2', otherID!);
+		let arr2 = await data.supabase
+			.from('conversations')
+			.select()
+			.eq('user2', myID!)
+			.eq('user1', otherID!);
 
+		console.log('arr1', arr1);
+		console.log('arr2', arr2);
 
-		return
-		//creating conversation
-		console.log(conversations);
+		if (arr1.data!.length == 0 && arr2.data!.length == 0) {
+			console.log('creating new conversation');
+			const { error } = await data.supabase
+				.from('conversations')
+				.insert({ user1: myID, user2: otherID });
+
+			messages = null;
+
+			return;
+		} else {
+			let conversation;
+			if (arr1.data != null) {
+				conversation = arr1.data[0];
+			} else if (arr2.data != null) {
+				conversation = arr2.data[0];
+			} else {
+				throw new Error('something went wrong switching conversations');
+			}
+
+			console.log(conversation);
+
+			console.log('conversation exists, grabbing messages!');
+			messages = await data.supabase
+				.from('messages')
+				.select()
+				.eq('conversation_id', conversation!.id);
+			return;
+		}
 	};
 </script>
 
 <main class="flex justify-center items-center">
 	<div id="contacts-list" class="basis-1/4">
 		<div id="lol" class=" container h-[100vh] p-2 overflow-auto">
-			{#if data.allUsers}
+			{#if data.otherUsers}
 				<ul>
-					{#each data.allUsers as user}
+					{#each data.otherUsers as user}
 						<li>
 							<button
 								on:click={() => {
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -8,16 +8,16 @@ export const load = async ({ parent }) => {
   }
   const { data: tableData } = await supabase.from('countries').select('*')
 
-  const {data: allUsers} = await supabase.from('users').select('*')
+  const {data: otherUsers} = await supabase.from('users').select().neq('id', session.user.id)
 
   const {data: userData} = await supabase.from('users').select().eq('id', session.user.id)
 
-
+  console.log("others", otherUsers)
 
   return {
     user: session.user,
     tableData,
-    allUsers,
+    otherUsers,
     userData,
   }
 }
